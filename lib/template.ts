71a844c7482b580de4d49fb164086d57bc6b62b3
sunnybyeon <sunnybyeon2631@gmail.com>
2022-01-09 18:08:18 +0900

    created main.css with styles for all pages

--- lib/template.ts
+++ lib/template.ts
@@ -20,6 +20,7 @@ export const layouts = {
                     <link rel="preconnect" href="https://fonts.googleapis.com">
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet">
+                    <link rel="stylesheet" href="/styles/main.css">
                     ${cssLink}
                     ${jsScript}
                     <title>${title}</title>
